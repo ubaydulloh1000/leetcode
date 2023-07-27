3158246bf86c1f0036ae2eeb07307eda33e4a8b4
Ubaydulloh Kuchkarov <ubaydulloh1000@mail.ru>
2023-07-27 12:58:02 +0500

    some changes

--- Grokking Algorithms/recursion/recursion.py
+++ Grokking Algorithms/recursion/recursion.py
@@ -1,11 +1,12 @@
-
 def factorial(n):
-	if n == 1:
-		return 1
-	return n * factorial(n-1)
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
 
 def main():
-	print(factorial(997))
+    print(factorial(997))
+
 
 if __name__ == '__main__':
-	main()
+    main()

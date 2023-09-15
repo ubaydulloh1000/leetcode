cc547b02c22ab0eeca63a84a2f128964ed6337c9
Ubaydulloh <ubaydulloh1000@mail.ru>
2023-09-15 17:53:42 +0500

    medium: max leetcode 75 days

--- leetcode/medium-max-consicutive-ones-3.py
+++ leetcode/medium-max-consicutive-ones-3.py
@@ -6,31 +6,63 @@ class Solution:
         n = len(nums)
         
         max_ones = 0
-        temp = 0
-        temp_k = 0  # pass up to 3 0's
+        zero_count = 0  # pass up to 3 0's
+        left = 0
         
-        for i in range(n):
-            if nums[i]:
-                temp += 1
-            else:
-                if temp_k < k:
-                    temp += 1
-                    temp_k += 1
-                else:
-                    temp = 0
-                    temp_k = 0
-            max_ones = max(temp, max_ones)
-            print("TEMP: ", temp)
-            print("MAX ONES: ", max_ones)
+        for right in range(n):
+            if nums[right] == 0:
+                zero_count += 1
+                
+                while zero_count > k:
+                    if nums[left] == 0:                
+                        zero_count -= 1
+                    left += 1
 
+            max_ones = max(right-left+1, max_ones)
         return max_ones
 
 
+class Solution2:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        
+        max_ones = 0
+        zero_count = 0  # pass up to k 0's
+        left, right = 0, 0
+        increment_0 = True
+
+        while right < n:
+            
+            if nums[right] == 1:
+                right += 1
+            else:
+                if increment_0:
+                    zero_count += 1
+                    
+                if zero_count > k:
+                    increment_0 = False
+                    
+                    if nums[left] == 0:
+                        zero_count-=1
+                    left+=1
+                else:
+                    right += 1
+                    increment_0 = True
+
+            max_ones = max(max_ones, right-left)
+        return max_ones
+
+
+
 def main():
+    # nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
     nums = [1,1,1,0,0,0,1,1,1,1,0]
     k = 2
-    r = Solution().longestOnes(nums, k)
-    print(r)
+    r1 = Solution().longestOnes(nums, k)
+    r2 = Solution2().longestOnes(nums, k)
+
+    print(r1)
+    print(r2)
 
 if __name__ == '__main__':
     main()
